0dc5eb7aa35139c7e5f4754c5d0c1542b49b9995
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-09 15:25:26 +0100

    refacto test action_change

--- feedo/action_change.py
+++ feedo/action_change.py
@@ -1,9 +1,6 @@
 from .abstract_action import AbstractAction
-from .event import Event
 import time
-import unittest
 from .hash_storage import HashStorage
-import os
 
 # This action will monitor a certain field and match if that field changes. The field must change with respect to the last event with the same query_key.
 
@@ -73,61 +70,3 @@ class ActionChange(AbstractAction):
 
     def update(self):
         self._state.store()
-
-
-
-TEST_DB = "/tmp/test.json"
-class TestActionChange(unittest.TestCase):
-    def setUp(self):
-        try:
-            os.remove(TEST_DB)
-        except:
-            pass
-
-    def test_no_change(self):
-        action = ActionChange("*", "alert.change", {"title":"something changed !"}, "c_key", "q_key")
-        event = Event("my_tag", 123456789, {"q_key": "XXXX", "c_key":True})
-        action.do(event)
-        event = Event("my_tag", 123456790, {"q_key": "XXXX", "c_key":True})
-        result = action.do(event)
-
-        self.assertEqual(result, event)
-
-    def test_change(self):
-        action = ActionChange("*", "alert.change", {"title":"something changed !"}, "c_key", "q_key")
-        event = Event("my_tag", 123456789, {"q_key": "XXXX", "c_key":True})
-        action.do(event)
-        event = Event("my_tag", 123456790, {"q_key": "XXXX", "c_key":False})
-        result = action.do(event)
-
-        self.assertEqual(result[1].tag, "alert.change")
-
-    def test_change_not_ignore_null(self):
-        action = ActionChange("*", "alert.change", {"title":"something changed !"}, "c_key", "q_key", ignore_null=False)
-        event = Event("my_tag", 123456789, {"q_key": "XXXX", "c_key":True})
-        action.do(event)
-        event = Event("my_tag", 123456790, {"q_key": "XXXX"})
-        result = action.do(event)
-        
-        self.assertEqual(result[1].tag, "alert.change")
-
-    def test_change_ignore_query_key(self):
-        action = ActionChange("*", "alert.change", {"title":"something changed !"}, "c_key", "q_key")
-        event = Event("my_tag", 123456789, {"c_key":True}) # missing q_key, ignored
-        action.do(event)
-        event = Event("my_tag", 123456790, {"q_key": "XXXX", "c_key":False})
-        result = action.do(event)
-        
-        self.assertEqual(result, event)
-
-    def test_store_and_load(self):
-        old_action = ActionChange("*", "alert.change", {"title":"something changed !"}, "c_key", "q_key", db_path=TEST_DB)
-        event = Event("my_tag", 123456789, {"q_key": "XXXX", "c_key":True})
-        old_action.do(event)
-        old_action.update()
-
-        action = ActionChange("*", "alert.change", {"title":"something changed !"}, "c_key", "q_key", db_path=TEST_DB)
-        event = Event("my_tag", 123456790, {"q_key": "XXXX", "c_key":False})
-        result = action.do(event)
-
-        self.assertEqual(result[1].tag, "alert.change")
