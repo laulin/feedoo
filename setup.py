b52fe128a8c23bb1d79c8f9c515b801f267f240a
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-09 14:45:46 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+setup(name='feedo',
+    version='0.1.0',
+    url='https://github.com/laulin/feedo',
+    license='GPLV3',
+    author='Laurent MOULIN',
+    author_email='gignops@gmail.com',
+    description='General purpose data processor',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: GNU GPL V3 License",
+       "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+      entry_points={
+            'console_scripts': [ 
+            'feedo = feedo.feedo:main' 
+            ] 
+      }
+)
