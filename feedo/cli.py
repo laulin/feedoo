048cd129918a2fc0a05ebf19c4e0d4ff46c24972
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-09 15:51:29 +0100

    refacto test cli

--- feedo/cli.py
+++ feedo/cli.py
@@ -1,5 +1,4 @@
 import argparse
-import unittest
 
 
 def get_args(argv=None):
@@ -7,12 +6,3 @@ def get_args(argv=None):
     parser.add_argument("-c", "--config", help="set the configuration file path", default="/etc/feed/default.yaml", dest="configuration_path")
     parser.add_argument('--verbose', '-v', action='count', default=0, help="verbose", dest="verbosity")
     return parser.parse_args(argv)
-
-class TestCli(unittest.TestCase):
-    def test_cli_conf_file(self):
-        args = get_args(["-c", "/etc/conf.yaml", "-vv"])
-        self.assertEqual(args.configuration_path, "/etc/conf.yaml")
-
-    def test_cli_verbose(self):
-        args = get_args(["-c", "/etc/conf.yaml", "-vv"])
-        self.assertEqual(args.verbosity, 2)
---
+++ tests/test_cli.py
@@ -0,0 +1,12 @@
+import unittest
+from feedo.cli import get_args
+
+
+class TestCli(unittest.TestCase):
+    def test_cli_conf_file(self):
+        args = get_args(["-c", "/etc/conf.yaml", "-vv"])
+        self.assertEqual(args.configuration_path, "/etc/conf.yaml")
+
+    def test_cli_verbose(self):
+        args = get_args(["-c", "/etc/conf.yaml", "-vv"])
+        self.assertEqual(args.verbosity, 2)
