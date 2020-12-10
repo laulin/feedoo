de36e227c5b3283e8088cebacf7f102d82d487ab
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-10 10:37:19 +0100

    move dummy to input

---
+++ feedo/input/input_dummy.py
@@ -0,0 +1,29 @@
+from feedo.abstract_action import AbstractAction
+from feedo.event import Event
+import time
+
+# provide a fake source of event for test
+
+class InputDummy(AbstractAction):
+    def __init__(self, tag, data):
+        AbstractAction.__init__(self)
+        self._tag = tag
+        self._data = data
+        
+    def do(self, event):
+        return event
+
+    def update(self):
+        if self._data is None:
+            return
+        if isinstance(self._data, dict):
+            event = Event(self._tag, int(time.time()), self._data)
+            self.call_next(event)
+            self._data = None
+            return
+        if isinstance(self._data, list):
+            for data in self._data:
+                event = Event(self._tag, int(time.time()), data)
+                self.call_next(event)
+                
+            self._data = None
