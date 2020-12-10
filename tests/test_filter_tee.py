a6996f4ab604a5422341fd4966576c163b7f4cb6
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-10 10:48:57 +0100

    move tee to filter

--- tests/test_filter_tee.py
+++ tests/test_filter_tee.py
@@ -1,10 +1,10 @@
-from feedo.action_tee import ActionTee
+from feedo.filter.filter_tee import FilterTee
 from feedo.event import Event
 import unittest
 
-class TestActionTee(unittest.TestCase):
+class TestFilterTee(unittest.TestCase):
     def test_1(self):
-        action = ActionTee("*", "t")
+        action = FilterTee("*", "t")
         event = Event("my_tag", 123456789, {"t": "2020-10-22T08:50:20+00:00"})
         result = action.do(event)
         
