f7da72b4b1d1b51d1791ab1e81d19fa883510ccd
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-10 10:28:51 +0100

    move date to filter

---
+++ feedo/filter/filter_date.py
@@ -0,0 +1,27 @@
+from chronyk import Chronyk
+from feedo.abstract_action import AbstractAction
+from feedo.event import Event
+
+# parse a date field and transform it
+
+class FilterDate(AbstractAction):
+    def __init__(self, match, key, format=None):
+        AbstractAction.__init__(self, match)
+        self._key = key
+        self._format = format
+
+    def do(self, event):
+        record = dict(event.record)
+        time = Chronyk(record[self._key])
+        if self._format is None:
+            record[self._key] = time.timestamp()
+        else:
+            record[self._key] = time.timestring(self._format)
+
+        return Event(event.tag, event.timestamp, record)
+
+
+
+
+
+
--- tests/test_filter_date.py
+++ tests/test_filter_date.py
@@ -1,12 +1,12 @@
-from feedo.action_date import ActionDate
+from feedo.filter.filter_date import FilterDate
 from feedo.event import Event
 import unittest
 
 # parse a date field and transform it
 
-class TestActionDate(unittest.TestCase):
+class TestFilterDate(unittest.TestCase):
     def test_1(self):
-        action = ActionDate("*", "t")
+        action = FilterDate("*", "t")
         event = Event("my_tag", 123456789, {"t": "2020-10-22T08:50:20+00:00"})
         result = action.do(event)
         expected = {"t": 1603356620.0}
@@ -14,7 +14,7 @@ class TestActionDate(unittest.TestCase):
         self.assertEqual(result.record, expected)
 
     def test_2(self):
-        action = ActionDate("*", "t", "%Y-%m-%d")
+        action = FilterDate("*", "t", "%Y-%m-%d")
         event = Event("my_tag", 123456789, {"t": 1603356620.0})
         result = action.do(event)
         expected = {"t": "2020-10-22"}
