d19b2422ff3627da4110264a5e0926372a48be9b
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-18 11:29:54 +0100

    post pone dabase connection to allow right drop

--- feedoo/hash_storage.py
+++ feedoo/hash_storage.py
@@ -17,19 +17,27 @@ class HashStorage(MutableMapping):
             path = ":memory:"
 
         self._table_name = table_name
-        self._connection = sqlite3.connect(path)
-        self._create_table()
+        self._connection = None
         self._timeout = timeout
+        self._path = path
+
+    def _get_connection(self):
+        # post pone the connection to allow right drop
+        if self._connection is None:
+            self._connection = sqlite3.connect(self._path)
+            self._create_table()
+
+        return self._connection
 
     def _create_table(self):
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS {} (key BLOB UNIQUE, timestamp INTEGER, value BLOB);".format(self._table_name))
-        self._connection.commit()
+        self._get_connection().commit()
 
     def get_timeout(self, _time=time):
         timestamp = int(_time()) - self._timeout
         
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT key FROM {} WHERE timestamp < ?'.format(self._table_name), (timestamp,))
         row = cursor.fetchall()
 
@@ -38,7 +46,7 @@ class HashStorage(MutableMapping):
     def __getitem__(self, key):
         blob_key = pickle.dumps(key)
 
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT value FROM {} WHERE key = ?'.format(self._table_name), (blob_key,))
         row = cursor.fetchone()
         if row is None:
@@ -51,35 +59,35 @@ class HashStorage(MutableMapping):
         blop_value = pickle.dumps(value)
         blob_key = pickle.dumps(key)
         fields = (blob_key, timestamp, blop_value)
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('''DELETE FROM {} WHERE key = ?'''.format(self._table_name), (blob_key,))
         cursor.execute('''INSERT INTO {}(key, timestamp, value) VALUES(?, ?, ?)'''.format(self._table_name), fields)
 
-        self._connection.commit()
+        self._get_connection().commit()
 
     def __delitem__(self, key):
         blob_key = pickle.dumps(key)
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('''DELETE FROM {} WHERE key = ?'''.format(self._table_name), (blob_key,))
 
-        self._connection.commit()
+        self._get_connection().commit()
 
     def __iter__(self):
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT key FROM {}'.format(self._table_name))
         rows = cursor.fetchall()
 
         return map(lambda x : pickle.loads(x[0]), rows)
     
     def __len__(self):
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT count(value) FROM {}'.format(self._table_name))
         row = cursor.fetchone()
         return row[0]
 
     def __repr__(self):
         output = "key;timestamp;value\n"
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT * FROM {}'.format(self._table_name))
         rows = cursor.fetchall()
 
@@ -88,21 +96,21 @@ class HashStorage(MutableMapping):
         return output + "\n".join(tmp)
 
     def keys(self):
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT key FROM {}'.format(self._table_name))
         rows = cursor.fetchall()
 
         return map(lambda x : pickle.loads(x[0]), rows)
 
     def values(self):
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT value FROM {}'.format(self._table_name))
         rows = cursor.fetchall()
 
         return map(lambda x : pickle.loads(x[0]), rows)
 
     def items(self):
-        cursor = self._connection.cursor()
+        cursor = self._get_connection().cursor()
         cursor.execute('SELECT key,value FROM {}'.format(self._table_name))
         rows = cursor.fetchall()
 
