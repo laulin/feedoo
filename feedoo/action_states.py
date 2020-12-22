e3b57989b82391d4622d87041e23449a3819e364
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-22 16:21:20 +0100

    add actions states

---
+++ feedoo/action_states.py
@@ -0,0 +1,33 @@
+class ActionStates:
+    def __init__(self, action_class_name):
+        self._action_class_name = action_class_name
+        self._stats = {
+            "in":dict(), # what action receive
+            "bypass":dict(), # what is refused by filter "match"
+            "do":dict(), # what do action receive
+            "ignore":dict(), # what do action ignored or failed events. Ignore must be added in the do function.
+            "out":dict() # what action produce (do or update)
+        }
+
+    def __getattribute__(self, name):
+        if name.startswith("add_"):
+            stat_name = name[4:]
+            if stat_name not in  object.__getattribute__(self, "_stats"):
+                raise AttributeError(name)
+
+            def generic_add(event):
+                if event is not None:
+                    tag = event.tag
+                    local_stats = object.__getattribute__(self, "_stats")[stat_name]
+                    if tag not in local_stats: 
+                        local_stats[tag] = 0
+                    local_stats[tag] += 1
+                    return local_stats[tag]
+                    
+            return generic_add
+
+        else:
+            return object.__getattribute__(self, name)
+
+    def get_states(self):
+        return self._stats
---
+++ tests/test_action_states.py
@@ -0,0 +1,31 @@
+from feedoo.action_states import ActionStates
+from feedoo.event import Event
+import unittest
+
+class TestActionStates(unittest.TestCase):
+    def test_1(self):
+        state = ActionStates("my_action")
+
+    def test_add_ok(self):
+        state = ActionStates("my_action")
+        event = Event("tag", 123456, {})
+        result = state.add_in(event)
+        expected = 1
+
+        self.assertEqual(result, expected)
+
+    def test_add_ko(self):
+        state = ActionStates("my_action")
+        event = Event("tag", 123456, {})
+
+        with self.assertRaises(AttributeError):
+            result = state.add_xxx(event)
+
+    def test_get_states(self):
+        state = ActionStates("my_action")
+        event = Event("tag", 123456, {})
+        state.add_in(event)
+        result = state.get_states()
+        expected = {'in': {'tag': 1}, 'bypass': {}, 'do': {}, 'ignore': {}, 'out': {}}
+
+        self.assertEqual(result, expected)
