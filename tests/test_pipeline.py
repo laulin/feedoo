eeae8055591e3fdb8d5c44ff7981218f6baf01b3
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-22 17:16:04 +0100

    add test to get states of pipeline

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,6 +1,7 @@
 from feedoo.pipeline import Pipeline
 from feedoo.plugins import Plugins
 import unittest
+from pprint import pprint
 
 DOCUMENT_TEST = '2020-10-22T08:50:03+00:00       pihole.dnsmasq  {"type":"dnsmasq","tag":"pihole.dnsmasq","logtime":"Oct 22 10:50:03","log":"reply weu1-authgw.cloudapp.net is 13.94.251.244","source":"jarvis-2"}'
 
@@ -29,3 +30,33 @@ class TestPipeline(unittest.TestCase):
         pipeline = Pipeline(self.actions)
         pipeline.create("simple", actions)
         pipeline.update()
+
+    def test_get_states(self):
+        actions = [
+            {"name" : "input_dummy", "tag" : "my_pypeline", "data":{"log":"my log"}},
+            {"name" : "output_stdout", "match" : "my_*"}
+        ]
+        pipeline = Pipeline(self.actions)
+        pipeline.create("simple", actions)
+        pipeline.update()
+
+        result = pipeline.get_states()
+
+        expected = ('simple',
+            [{'bypass': {},
+            'do': {},
+            'ignore': {},
+            'in': {},
+            'info': 'Dummy[my_pypeline]',
+            'name': 'InputDummy',
+            'out': {'my_pypeline': 1}},
+            {'bypass': {},
+            'do': {'my_pypeline': 1},
+            'ignore': {},
+            'in': {'my_pypeline': 1},
+            'info': 'Stdout[my_*]',
+            'name': 'OutputStdout',
+            'out': {}}])
+
+        self.assertEqual(result, expected)
+
