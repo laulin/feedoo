3a16717a2f956ab1e15a998c2512eae48a39d16a
lmoulin <lmoulin@spartan-conseil.fr>
2021-01-12 13:23:13 +0100

    add abstract output file class

---
+++ feedoo/abstract_output_file.py
@@ -0,0 +1,67 @@
+from chronyk import Chronyk
+import os
+import os.path
+import logging
+from contextlib import suppress
+from feedoo.abstract_action import AbstractAction
+from feedoo.event import Event
+from feedoo.hash_storage import HashStorage
+from time import time
+
+class AbstractOutputFile(AbstractAction):
+    def __init__(self, match, time_key, path_template, buffer_size=1000, timeout_flush=60, db_path=None):
+        AbstractAction.__init__(self, match)
+        self._time_key = time_key
+        self._path_template = path_template
+        self._buffer = HashStorage(db_path, timeout_flush)
+        self._buffer_size = buffer_size
+
+        self._last_flush = 0
+        self._timeout_flush = timeout_flush
+
+    def do(self, event):
+        record = event.record
+        time = Chronyk(record[self._time_key])
+        path = time.timestring(self._path_template)
+        path = path.format(**record)
+        if path not in self._buffer:
+            self._buffer[path] = list()
+
+        self._buffer[path].append(record)
+
+        if len(self._buffer[path]) > self._buffer_size:
+            self.flush_one(path)
+
+        return event
+
+    def finish(self):
+        self._log.debug("finish")
+
+        for k in tuple(self._buffer.keys()):
+            self._log.info("Flush (finish) {}".format(k))
+            self.flush_one(k)
+        self._buffer.dump()
+
+    def flush_one(self, path):
+        self._log.debug("flush to {}".format(path))
+
+        with suppress(FileExistsError):
+            directory = os.path.dirname(path)
+            os.makedirs(directory, 0o755)
+
+        with open(path, "a") as f:
+            values = self._buffer[path]
+            data = self.value_to_string(values)
+            f.write(data)
+            del self._buffer[path]
+
+    def update(self, _time=time):
+        if _time() - self._last_flush > self._timeout_flush:
+            self._last_flush = _time()
+            for path in tuple(self._buffer.keys()):
+                self._log.info("Flush {}".format(path))
+                self.flush_one(path)
+
+    def value_to_string(self, values):
+        raise NotImplemented()
+
--- feedoo/output/output_archive.py
+++ feedoo/output/output_archive.py
@@ -1,69 +1,12 @@
-from chronyk import Chronyk
-from collections import defaultdict
-import os
-import os.path
-import shutil
-import logging
-from contextlib import suppress
-from feedoo.abstract_action import AbstractAction
-from feedoo.event import Event
-from feedoo.hash_storage import HashStorage
-from time import time
+from feedoo.abstract_output_file import AbstractOutputFile
 import json
 
 # push document to archive files
 
-class OutputArchive(AbstractAction):
+class OutputArchive(AbstractOutputFile):
     def __init__(self, match, time_key, path_template, buffer_size=1000, timeout_flush=60, db_path=None):
-        AbstractAction.__init__(self, match)
-        self._time_key = time_key
-        self._path_template = path_template
-        self._buffer = HashStorage(db_path, timeout_flush)
-        self._buffer_size = buffer_size
+        AbstractOutputFile.__init__(self, match, time_key, path_template, buffer_size, timeout_flush, db_path)
 
-        self._last_flush = 0
-        self._timeout_flush = timeout_flush
-
-    def do(self, event):
-        record = event.record
-        time = Chronyk(record[self._time_key])
-        path = time.timestring(self._path_template)
-        path = path.format(**record)
-        if path not in self._buffer:
-            self._buffer[path] = list()
-
-        self._buffer[path].append(record)
-
-        if len(self._buffer[path]) > self._buffer_size:
-            self.flush_one(path)
-
-        return event
-
-    def finish(self):
-        self._log.debug("finish")
-
-        for k in tuple(self._buffer.keys()):
-            self._log.info("Flush (finish) {}".format(k))
-            self.flush_one(k)
-        self._buffer.dump()
-
-    def flush_one(self, path):
-        self._log.debug("flush to {}".format(path))
-
-        with suppress(FileExistsError):
-            directory = os.path.dirname(path)
-            os.makedirs(directory, 0o755)
-
-        with open(path, "a") as f:
-            values = self._buffer[path]
-            data = "\n".join(map(json.dumps, values)) + "\n"
-            f.write(data)
-            del self._buffer[path]
-
-    def update(self, _time=time):
-        if _time() - self._last_flush > self._timeout_flush:
-            self._last_flush = _time()
-            for path in tuple(self._buffer.keys()):
-                self._log.info("Flush {}".format(path))
-                self.flush_one(path)
+    def value_to_string(self, values):
+        return "\n".join(map(json.dumps, values)) + "\n"
 
